8f655ee8a6997c6f8ef2fd445cdff7be5ce8c711
Heying778 <yingyinghe2022@gmail.com>
2022-08-24 09:29:57 +0100

    all finished

--- src/test/java/floor/FloorTest.java
+++ src/test/java/floor/FloorTest.java
@@ -1,4 +1,32 @@
 package floor;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import vehicles.Hatchback;
+import vehicles.Vehicle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FloorTest {
+    private Floor floor;
+
+    @BeforeEach
+    public void setup(){
+        floor = new Floor(1);
+    }
+    @Test
+    public void cancountVehicles(){
+        int expected = 0;
+        int actual = floor.countVehicles();
+        assertEquals(expected, actual);
+    }
+
+    public void canAddVehicle(){
+        Vehicle vehicle = new Hatchback("MG","999","1");
+        floor.addVehicle(vehicle);
+        int expected = 1;
+        int actual = floor.addVehicle();
+        assertEquals(expected, actual);
+    }
+
 }
